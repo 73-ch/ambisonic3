db1f37ed70f9fc198a1196d6274b8cb0d9599c8f
nami634 <cigarbox.nami@gmail.com>
2018-09-21 20:35:56 +0900

    noise_testがクリックノイズを発生させる理由を探るテスト 結果：ScriptProcessorNodeの自作lowpass_filterが原因

---
+++ frontend/test_json/lowpass_test.js
@@ -0,0 +1,16 @@
+
+// this.noise_player.generateLowpassFilter();
+
+// this.nodes["source"].connect(this.noise_player.lowpass_filter);
+
+// this.noise_player.lowpass_filter.connect(this.nodes["gain"]);
+
+// this.nodes["source"].start();
+
+
+// 結果
+/*
+
+クリックノイズは時刻同期のズレではなく、フィルター自体のズレにあると考えられる
+
+ */
