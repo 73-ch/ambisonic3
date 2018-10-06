08f2620dd85a7daaccf559fac8924c5959310319
nami634 <cigarbox.nami@gmail.com>
2018-10-06 11:50:18 +0900

    samplerが進んだ

--- frontend/test_files/sequence_demo/sequence.js
+++ frontend/test_files/sequence_demo/sequence.js
@@ -1,27 +1,44 @@
 const position = this.position_manager.position;
 
-const num = position.x + position.y * 4;
+const num = position[0]*4 + position[1];
 
+console.log(num);
 
 const seconds_per_beat = 500;
 
-const schedule_ahead = 100;
+const schedule_ahead = 200;
 
 let next_note_time = $time;
-let note_num = 0;
+let note_num_16th = 0;
+let sequence_count = 0;
+// [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+let sequence = [
+    [["000_bar", "002_foo"],[],["002_foo"],["003"],[],["000_bar"],["002_foo"],[],["000_bar"],[],["000_bar"],["000_bar"],[],["000_bar"],["000_bar"],[]],
+    [[],["000_bar"],[],["003"],["003"],["003"],["000_bar"],[],[],[],["000_bar"],[],[],[],["000_bar"],[]]
+];
+
+clearInterval(this.intervals.sequence);
 
 
 this.intervals.sequence = setInterval(() => {
     if (this.time_sync.current_time > next_note_time + schedule_ahead) {
-        schedule(note_num, next_note_time);
+        schedule(sequence_count, note_num_16th, next_note_time);
         next_note_time += seconds_per_beat * 0.25;
-        note_num = (note_num + 1) % 16;
+        note_num_16th++;
+        if (note_num_16th >= 16) {
+            note_num_16th %= 16;
+            sequence_count = (sequence_count + 1) % sequence.length;
+        }
     }
 }, 100);
 
-const schedule = (note_num, time) => {
-    if (note_num % 4 === 0) {
-        playSample("000_bar", time);
+const schedule = (count, note_num, time) => {
+    const target = sequence[count][note_num];
+    if (!target) return;
+    if (target.length >= 1) {
+        for (let sample of target) {
+            playSample(sample, time);
+        }
     }
 };
 
@@ -40,4 +57,3 @@ const playSample = (buffer_name, time) => {
     this.playLoadedSource(bar_params, this.time_sync.getAudioTime(time));
 };
 
-// current_step = [];
