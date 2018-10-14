e02d452924e2353eb8ff74e5420a8d3f5d036758
nami634 <cigarbox.nami@gmail.com>
2018-10-14 13:42:19 +0900

    Sequencerクラスのテストを追加

---
+++ frontend/test_files/sequencer_test/sequencer_test.js
@@ -0,0 +1,11 @@
+this.sequencer.setBPM(120);
+
+// let sequence = [["hand0"],[],[],[],["hand0"],[],[],[],["hand0"],[],[],[],["hand0"],[],[],[]];
+
+// this.sequencer.addSequence(sequence);
+
+// this.sequencer.resume($time + 2000);
+
+// this.sequencer.removeSequence(0);
+
+// this.sequencer.clearSequence();
