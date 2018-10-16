c24bc7840be6c50f6929aaad4fd5c82f85ac98c5
nami634 <cigarbox.nami@gmail.com>
2018-10-16 18:18:18 +0900

    新しいVisualizerのテストを追加

---
+++ frontend/test_files/visual_test/visual_test.js
@@ -0,0 +1,15 @@
+
+// if (this.intervals.change_color) {
+//     clearInterval(this.intervals.change_color);
+// }
+
+// this.intervals.change_color = setInterval(()=> {
+//     this.visualizer.addColor([Math.random(),Math.random(),Math.random(),Math.random()], [-0.05 * Math.random(),-0.05 * Math.random(),-0.05 * Math.random()]);
+// }, 100);
+
+// !location.reload();
+
+this.visualizer.addColor([1.0,0.0,0.0, 0.5], [0.000000025,0.,0.]);
+this.visualizer.addColor([0.,1.0,0., 0.5], [0.,0.025,0.]);
+
+// !this.visualizer.clearColor();
