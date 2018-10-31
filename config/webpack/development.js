fc1a5c3473d36bf3ec74c1594aff10dfc6c174da
nami634 <cigarbox.nami@gmail.com>
2018-10-31 19:38:04 +0900

    glslify-import-loaderを追加

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,6 +1,12 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const environment = require('./environment')
+const environment = require('./environment');
+
+environment.loaders.append('glsl-import', {
+    test: /\.(glsl|frag|vert)$/,
+    use: 'glslify-import-loader',
+    exclude: /node_modules/
+});
 
 environment.loaders.append('raw', {
     test: /\.(glsl|frag|vert)$/,
