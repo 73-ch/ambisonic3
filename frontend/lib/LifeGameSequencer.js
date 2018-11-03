eda94ac58528e97ca4ef91b4c7c4486a595f166d
nami634 <cigarbox.nami@gmail.com>
2018-11-04 02:41:56 +0900

    ライフゲームから音を作成するように

---
+++ frontend/lib/LifeGameSequencer.js
@@ -0,0 +1,167 @@
+import clone from 'clone';
+
+const emp_seq = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
+
+const Sequencer = class {
+    constructor(_context, _time_sync, _life_fame, _tk) {
+        this.context = _context;
+        this.time_sync = _time_sync;
+        this.life_game = _life_fame;
+
+        this.sequences = [];
+        this.light_sequences = [];
+
+        this.next_note_time = 0;
+        this.ahead = 200;
+        this.sequence_count = 0;
+        this.note_num_16th = 0;
+
+        this.setBPM(64);
+
+        this.interval = null;
+        this.interval_span = 25;
+
+        this.status = false;
+
+        this.tk = _tk;
+    }
+
+    addSequence(...args) {
+        console.log(args);
+        let light_pattern;
+
+        if (args.length === 2) {// index, sequence
+            light_pattern = this.createDefaultLightPattern(args[1], [1, 1, 1, 1], [.05, .05, .05]);
+        } else if (args.length === 3) {// index, sequence, light_sequence
+            light_pattern = args[2];
+        } else if (args.length === 4) {// index, sequence, light, sub
+            light_pattern = this.createDefaultLightPattern(args[1], args[2], args[3]);
+        }
+
+        this.addPattern(this.sequences, args[0], args[1]);
+        this.addPattern(this.light_sequences, args[0], light_pattern);
+    }
+
+
+    addPattern(type, seq_index, pattern) {
+        while (type.length <= seq_index) type.push(new Array(16));
+
+        for (let i = 0; i < pattern.length; i++) {
+            if (pattern[i]) type[seq_index][i].push(pattern[i]);
+        }
+
+    }
+
+    createDefaultLightPattern(seq, col, sub) {
+        console.log(seq, col, sub);
+        let ret_pattern = [];
+        for (let i = 0; i < seq.length; i++) {
+            let p = [];
+
+            if (seq[i]) p = [col, sub];
+
+            ret_pattern.push(p);
+        }
+
+        return ret_pattern;
+    }
+
+    removeSequence(index) {
+        this.sequences.splice(index, 1);
+    }
+
+    clearSequence() {
+        this.sequences = [];
+        this.light_sequences = [];
+    }
+
+    setBPM(bpm) {
+        this.seconds_per_beat = 60000 / bpm;
+    }
+
+    resume(time) {
+        if (this.status === true) this.stop();
+        this.status = true;
+        this.next_note_time = time;
+        // this.note_num_16th = 0;
+        // this.sequence_count = 0;
+
+        // if (this.interval) clearInteral(this.interval);
+
+        this.interval = setInterval(() => {
+            this.main();
+        }, this.interval_span);
+    }
+
+    stop() {
+        if (this.status === true) {
+            clearInterval(this.interval);
+            this.status = false;
+        }
+    }
+
+    main() {
+        if (this.time_sync.current_time > this.next_note_time + this.ahead) {
+            if (this.note_num_16th % 4 === 0) {
+                this.schedule(this.sequence_count, this.note_num_16th, this.next_note_time);
+            }
+
+
+            // next_note_time,
+            this.next_note_time += this.seconds_per_beat * 0.0625;
+            this.note_num_16th++;
+
+            if (this.note_num_16th >= 16) {
+                this.note_num_16th %= 16;
+                this.sequence_count = (this.sequence_count + 1) % this.sequences.length | 0;
+                console.log(this.sequence_count);
+            }
+        }
+    }
+
+    schedule(count, note_num, time) {
+        this.life
+
+        const target = this.sequences[count][note_num];
+        if (!target) return;
+        if (target.length >= 1) {
+            console.log(target);
+            for (let sample of target) {
+                this.playSample(sample, time);
+            }
+        }
+
+        const lights = this.light_sequences[count][note_num];
+        if (!lights) return;
+        if (lights.length >= 1) {
+            for (let c of lights) {
+                if (c.length === 2) this.flashDisplay(c, time);
+            }
+        }
+
+    }
+
+    playSample(buffer_name, time) {
+        const bar_params = {
+            "name": "source_" + buffer_name + "_" + time,
+            "node_type": "buffer_source",
+            "out": "gain",
+            "params": {
+                "buffer": buffer_name,
+                "loop": false
+            }
+        };
+
+        // console.log( this.time_sync.getAudioTime(time));
+        this.tk.playLoadedSource(bar_params, this.time_sync.getAudioTime(time));
+    }
+
+    flashDisplay(couple, time) {
+        // visualizer
+        setTimeout(() => {
+            this.visualizer.addColor(couple[0].concat(), couple[1].concat());
+        }, time - this.time_sync.current_time);
+    }
+};
+
+export default Sequencer;
--- frontend/test_files/life_game/script.js
+++ frontend/test_files/life_game/script.js
@@ -6,29 +6,45 @@ import update_frag from './shader/fragment.frag'
 import render_frag from './shader/render_frag.glsl'
 
 const LifeGame = class extends BaseScene {
-    constructor(_position = [0,0,0], _renderer) {
+    constructor(_position = [0, 0, 0], _renderer, _time_sync, _context, _tk) {
         super();
+        // graphic params
         this.size = {x: 100, y: 100};
 
         this.position = _position;
-        this.size_per_device = {x:50, y:50};
+        this.size_per_device = {x: 5, y: 5};
+
+        this.init_seed = Math.random();
 
         this.age = 0;
-
-        this.count = 0;
 
         this.renderer = _renderer;
 
         this.pixels = new Uint8Array(this.size_per_device.x * this.size_per_device.y * 4);
+
+        // audio params
+        this.context = _context;
+        this.tk = _tk;
+
+        // schedule params
+        this.time_sync = _time_sync;
+
+        this.next_time = 0;
+        this.ahead = 150;
+        this.count = 0;
+
+        this.spa = 200; // m_seconds per age
+
+        this.status = false;
 
         this.initWholeMap();
         this.setDefaultWholeMap();
 
         this.initUpdateScene();
 
+        this.initRenderScene();
 
         // debug
-        // this.initRenderScene();
         // setTimeout(()=> {
         //     setInterval(() => {
         //         this.incrementAge();
@@ -41,6 +57,54 @@ const LifeGame = class extends BaseScene {
         //         this.swapBuffer();
         //     }, 100)
         // }, 3100);
+    }
+
+    update() {
+        if (!this.status) return;
+
+        if (this.time_sync.current_time > this.next_time + this.ahead) {
+            this.count++;
+
+            this.incrementAge();
+            let pixel_count = 0;
+            for (let i = 0; i < this.pixels.length; i+=4) {
+                pixel_count += this.pixels[i];
+            }
+            pixel_count /= 255;
+
+            console.log(pixel_count);
+
+            if (pixel_count > 0 && pixel_count < 8) {
+                const buffer_name = ('00' + pixel_count);
+                const bar_params = {
+                    "name": "source_" + buffer_name + "_" + this.next_time,
+                    "node_type": "buffer_source",
+                    "out": "gain",
+                    "params": {
+                        "buffer": buffer_name,
+                        "loop": false
+                    }
+                };
+
+                this.tk.playLoadedSource(bar_params, this.time_sync.getAudioTime(this.next_time));
+            }
+
+
+            setTimeout(() => {
+                this.swapBuffer();
+            }, this.next_time - this.time_sync.current_time);
+
+            this.next_time += this.spa;
+        }
+    }
+
+    resume(time) {
+        this.status = true;
+        this.next_time = time;
+    }
+
+    stop() {
+        this.status = false;
     }
 
     initWholeMap() {
@@ -64,7 +128,7 @@ const LifeGame = class extends BaseScene {
         // 一番最初のlifegameのマップへの書き込み
         let init_scene = new THREE.Scene();
 
-        this.whole_cam = new THREE.OrthographicCamera(this.size.x / -2.,this.size.x / 2., this.size.y / 2., this.size.y / -2., 0, 100);
+        this.whole_cam = new THREE.OrthographicCamera(this.size.x / -2., this.size.x / 2., this.size.y / 2., this.size.y / -2., 0, 100);
         init_scene.add(this.whole_cam);
 
         // lifegame全体のマテリアルと
@@ -78,7 +142,7 @@ const LifeGame = class extends BaseScene {
                 },
                 "seed": {
                     type: 'f',
-                    value: Math.random()
+                    value: this.init_seed
                 }
             },
             vertexShader: vertex,
@@ -150,12 +214,9 @@ const LifeGame = class extends BaseScene {
 
         //
         for (let tri of this.display_geometry.faceVertexUvs[0]) {
-            console.log(tri);
             for (let v of tri) {
-                console.log(v);
                 v.x = (v.x + this.position[0]) * this.size_per_device.x / this.size.x;
                 v.y = (v.y + this.position[1]) * this.size_per_device.y / this.size.y;
-                console.log(v);
             }
         }
 
@@ -193,9 +254,6 @@ const LifeGame = class extends BaseScene {
         // let pixels = new Uint8Array(this.size_per_device.x * this.size_per_device.y * 4);
         const gl = this.renderer.getContext();
         gl.readPixels(this.position[0], this.position[1], this.size_per_device.x, this.size_per_device.y, gl.RGBA, gl.UNSIGNED_BYTE, this.pixels);
-        console.log(this.pixels);
-        this.countRendered();
-
         // this.swapBuffer();
     }
 
@@ -203,10 +261,6 @@ const LifeGame = class extends BaseScene {
         const temp_buffer = this.back_buffer;
         this.back_buffer = this.front_buffer;
         this.front_buffer = temp_buffer;
-    }
-
-    countRendered(pixels) {
-        pixels.reduce((b,a) => b + a);
     }
 
     reset() {
@@ -219,20 +273,6 @@ const LifeGame = class extends BaseScene {
         this.cam.bottom = height / 2.;
 
     }
-
-
-    judgeCell(age, tars) {
-        let count = 0;
-
-        for (let i = 0, len_i = tars.length; i < len_i; i++) {
-            for (let j = 0, len_j = tars[0].length; j < len_j; j++) {
-                count = XYUtils.countAround([i, j], tars);
-
-            }
-        }
-
-    }
-
 };
 
 export default LifeGame;
